fcc58c9a43173c029ac24e42e3ca62ef3308783d
eXory2024 <exory@anio.software>
2024-11-09 19:49:57 +0100

    Update fourtune.config.mjs.

--- fourtune.config.mjs
+++ fourtune.config.mjs
@@ -1,18 +1,4 @@
-import {generateFromTemplate} from "fourtune/autogenerate"
-
-const asyncToSync = {
-	"import {readFileInChunks} from \"@anio-fs/int--read-file-in-chunks\"": "import {readFileInChunksSync} from \"@anio-fs/int--read-file-in-chunks\"",
-	"export default async function": "export default function",
-	"await readFileInChunks": "readFileInChunksSync",
-	"await file.readNextChunk()": "file.readNextChunk()"
-}
-
 export default {
 	realm: "js",
-	type: "package",
-
-	autogenerate: {
-		"sync.mjs": generateFromTemplate("src/template.mjs", asyncToSync),
-		"async.mjs": generateFromTemplate("src/template.mjs", {})
-	}
+	type: "package:async/sync"
 }
