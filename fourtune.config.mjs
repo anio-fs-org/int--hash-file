c3da496531cda38116a0f0d96fd5a0daffc7d7df
eXory2024 <exory@anio.software>
2024-11-09 20:05:18 +0100

    Update fourtune.config.mjs.

--- fourtune.config.mjs
+++ fourtune.config.mjs
@@ -1,4 +1,12 @@
 export default {
 	realm: "js",
-	type: "package:async/sync"
+	type: "package:async/sync",
+
+	target: {
+		function_name: "hashFile",
+
+		dependencies: {
+			"@anio-fs/int--read-file-in-chunks": "readFileInChunks"
+		}
+	}
 }
